f1b5d7ef27c09c0b33d2c90b3fc952aab707656b
Khan Md Shibli Nomani <101654553+Shibli-Nomani@users.noreply.github.com>
2023-09-02 02:43:36 +0600

    docker-airflow-proper

--- dags/test.py
+++ dags/test.py
@@ -1,30 +1,25 @@
-from datetime import datetime
+# airflow
 from airflow import DAG
-from airflow.operators.pyton_operator import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
 
-#define work function
+
+# define work function
 def print_hw():
     print("hello")
+
+
 def print_gb():
     print("Goodbye")
+# prepare dag
+test_dag = DAG("test_DAG", schedule_interval=None, start_date=datetime(2023, 9, 1))
 
-#prepare dag
-test_dag = DAG("test_DAG", 
-               schedule_interval = None,
-               start_date = datetime(2023,9,1))
-
-#prepare task id
-TASK_ONE = 'task_one'
-TASK_TWO = 'task_two'
+# prepare task id
+TASK_ONE = "task_one"
+TASK_TWO = "task_two"
 
 with test_dag:
-    first_task = PythonOperator(
-        task_id = TASK_ONE,
-        python_callable = print_hw
-    )
-    second_task = PythonOperator(
-        task_id = TASK_TWO,
-        python_callable = print_gb
-    )
-    #task direction
+    first_task = PythonOperator(task_id=TASK_ONE, python_callable=print_hw)
+    second_task = PythonOperator(task_id=TASK_TWO, python_callable=print_gb)
+    # task direction
     first_task >> second_task
